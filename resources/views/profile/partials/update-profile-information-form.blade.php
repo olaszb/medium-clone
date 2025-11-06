9c52fa09a5a78df5f9e90cac5fc9ff4f60ed9f69
purgalo <olasz.g48@gmail.com>
2025-11-06 18:36:47 +0100

    added the new fields to the user models fillable and the update profile page

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -18,9 +18,21 @@
         @method('patch')
 
         <div>
+            <x-input-label for="image" :value="__('Avatar')" />
+            <x-text-input id="image" class="block mt-1 w-full" type="file" name="image" :value="old('image')" />  
+            <x-input-error :messages="$errors->get('image')" class="mt-2" />
+        </div>
+
+        <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
+            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
+        </div>
+
+        <div>
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input id="username" name="username" type="text" class="mt-1 block w-full" :value="old('username', $user->username)" required autocomplete="username" />
+            <x-input-error class="mt-2" :messages="$errors->get('username')" />
         </div>
 
         <div>
@@ -45,6 +57,14 @@
                     @endif
                 </div>
             @endif
+
+            <div class="mt-4">
+                <x-input-label for="bio" :value="__('Bio')" />
+                <x-input-textarea id="bio" class="block mt-1 w-full" name="bio">
+                    {{ old('bio') }}
+                </x-input-textarea>
+                <x-input-error :messages="$errors->get('bio')" class="mt-2" />
+            </div>
         </div>
 
         <div class="flex items-center gap-4">
