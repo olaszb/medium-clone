09144fcb81b608fbc1b0652304054077b84fa7e1
purgalo <olasz.g48@gmail.com>
2025-11-06 18:39:14 +0100

    made a new request for the post creation and put the validation logic there

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostCreateRequest;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -38,15 +39,9 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PostCreateRequest $request)
     {
-        $data = $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
-            'title' => 'required',
-            'content' => 'required',
-            'category_id' => ['required', 'exists:categories,id'],
-            'published_at' => ['nullable', 'datetime']
-        ]);
+        $data = $request->validated();
 
         $image = $data['image'];
         unset($data['image']);
