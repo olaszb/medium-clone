bb6580cbe4a22fea8e1ba55c87b929d1fae479c8
purgalo <olasz.g48@gmail.com>
2025-11-06 13:48:21 +0100

    added create post button and changed colors of buttons

--- resources/views/components/category-tabs.blade.php
+++ resources/views/components/category-tabs.blade.php
@@ -1,7 +1,7 @@
 <ul
     class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
     <li class="me-2">
-        <a href="#" class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active"
+        <a href="#" class="inline-block px-4 py-2 text-white bg-gray-800 rounded-lg active"
             aria-current="page">All</a>
     </li>
     @foreach ($categories as $category)
