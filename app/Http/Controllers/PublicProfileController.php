fb622e7cc478d63286475d1c0f557dc5c660c31f
purgalo <olasz.g48@gmail.com>
2025-11-20 21:06:43 +0100

    created follower model,migration and added followers and following methods and relations to the models

--- app/Http/Controllers/PublicProfileController.php
+++ app/Http/Controllers/PublicProfileController.php
@@ -9,6 +9,7 @@ class PublicProfileController extends Controller
 {
     public function show(Request $request, User $user){
         $posts = $user->posts()->latest()->paginate(10);
+
         return view('profile.show', [
             'user'=> $user,
             'posts'=>$posts,
