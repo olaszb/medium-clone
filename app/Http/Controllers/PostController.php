ba2745d7519e87ffd408444a40eba2ebe4613baa
purgalo <olasz.g48@gmail.com>
2025-11-23 19:08:59 +0100

    made it so image is optional for editing posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PostCreateRequest;
+use App\Http\Requests\PostUpdateRequest;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -100,7 +101,7 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(PostCreateRequest $request, Post $post)
+    public function update(PostUpdateRequest $request, Post $post)
     {
         if($post->user_id !== Auth::id()){
             abort(403);
